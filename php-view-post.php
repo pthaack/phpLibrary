55c7761d5051851c756368b6c319a500e861611b
Philip Young <pthaack@users.noreply.github.com>
2016-07-12 11:38:30 -0400

    Update php-view-post.php

--- php-view-post.php
+++ php-view-post.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- add this URI to the "action" parameter in a <FORM> tag. It returns the raw data posted to that form. -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
