7f94dc68f63f465c528f82c59637f5cdd43bf0ed
Philip Young <pthaack@users.noreply.github.com>
2016-07-12 11:34:27 -0400

    Update db-manager.php

--- db-manager.php
+++ db-manager.php
@@ -1,16 +1,17 @@
 <?php
+/// This file must be included and the function 'accessAuthenticate()' querried before any HTML is used, even a line feed.
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', '956997_example');
+define('DB_NAME', 'db_example');
 
 /** MySQL database username */
-define('DB_USER', '956997_master');
+define('DB_USER', 'db_master');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'lY9729NEVsbiKIs');
+define('DB_PASSWORD', 'db_master_pwd');
 
 /** MySQL hostname */
-define('DB_HOST', 'mariadb-091.wc2.dfw3.stabletransit.com');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -23,7 +24,7 @@ define('DB_COLLATE', '');
 //******** VERY IMPORTANT -- call to this function must be performed before all other functions, especially ones with TRACErs *********
 /// ToDo: connect to database
 function accessAuthenticate() {	 
-	$valid_passwords = array ("cinaed" => "password1", "orlaith" => "password2");
+	$valid_passwords = array ("user1" => "password1", "user2" => "password2");
 	$valid_users = array_keys($valid_passwords);
 	
 	$validated = false;
@@ -72,228 +73,6 @@ function dbAccessClose($objConn)
 /* /Close Database */
 
 // ToDo: Safely validate login information
-/*
-SELECT `strUserName`, `strFirstName`, `strLastName`, `blnAdmin`, `blnPermission` FROM `es_users_list`
-WHERE `strUserName`='cinaed' AND `strPassword`='password1'
-
-http://php.net/manual/en/mysqli.real-escape-string.php
-http://stackoverflow.com/questions/3683746/escaping-mysql-wild-cards
-
-SELECT `COLUMN_NAME` FROM `information_schema`.`COLUMNS`
-WHERE `TABLE_NAME` = `es_users_list'
-
-
-SELECT `TABLE_NAME`, `COLUMN_NAME`, `DATA_TYPE`, `CHARACTER_MAXIMUM_LENGTH`, `COLLATION_NAME`, `COLUMN_TYPE` FROM `COLUMNS`
-WHERE `TABLE_NAME` LIKE 'es_%'
-TABLE_NAME	COLUMN_NAME	DATA_TYPE	CHARACTER_MAXIMUM_LENGTH	COLLATION_NAME	COLUMN_TYPE	
- Current selection does not contain a unique column. Grid edit, checkbox, Edit, Copy and Delete features are not available.
- Showing rows 0 - 24 (25 total, Query took 0.0169 seconds.)
-SELECT `TABLE_NAME`, `COLUMN_NAME`, `DATA_TYPE`, `CHARACTER_MAXIMUM_LENGTH`, `COLLATION_NAME`, `COLUMN_TYPE` FROM `COLUMNS` WHERE `TABLE_NAME` LIKE 'es_%'
-
-TABLE_NAME
-COLUMN_NAME
-DATA_TYPE
-CHARACTER_MAXIMUM_LENGTH
-COLLATION_NAME
-COLUMN_TYPE
-es_users_list
-idUser
-int
-NULL
-NULL
-int(11)
-es_users_list
-strUserName
-varchar
-12
-ascii_general_ci
-varchar(12)
-es_users_list
-strFirstName
-varchar
-30
-utf8_general_ci
-varchar(30)
-es_users_list
-strLastName
-varchar
-30
-utf8_general_ci
-varchar(30)
-es_users_list
-strPassword
-varchar
-15
-ascii_general_ci
-varchar(15)
-es_users_list
-blnAdmin
-tinyint
-NULL
-NULL
-tinyint(1)
-es_users_list
-blnPermission
-tinyint
-NULL
-NULL
-tinyint(1)
-es_users_list
-blnCookie
-tinyint
-NULL
-NULL
-tinyint(1)
-es_users_list
-strEmail
-varchar
-45
-ascii_general_ci
-varchar(45)
-es_users_list
-strSMS
-varchar
-10
-ascii_general_ci
-varchar(10)
-es_venues_list
-idVenue
-int
-NULL
-NULL
-int(11)
-es_venues_list
-idUser
-int
-NULL
-NULL
-int(11)
-es_venues_list
-dteEntry
-timestamp
-NULL
-NULL
-timestamp
-es_venues_list
-strLocationName
-varchar
-50
-ascii_general_ci
-varchar(50)
-es_venues_list
-strAddress
-mediumtext
-16777215
-ascii_general_ci
-mediumtext
-es_venues_list
-strCity
-varchar
-50
-ascii_general_ci
-varchar(50)
-es_venues_list
-strProvince
-varchar
-2
-ascii_general_ci
-varchar(2)
-es_venues_list
-strPostalCode
-varchar
-10
-ascii_general_ci
-varchar(10)
-es_venues_list
-strGeolocationStyle
-varchar
-1
-ascii_general_ci
-varchar(1)
-es_venues_list
-strGeolocationPoints
-varchar
-250
-ascii_general_ci
-varchar(250)
-es_venues_list
-strPhoneNumber
-varchar
-20
-ascii_general_ci
-varchar(20)
-es_venues_list
-strContact
-varchar
-50
-ascii_general_ci
-varchar(50)
-es_venues_list
-strEmail
-varchar
-50
-ascii_general_ci
-varchar(50)
-es_venues_list
-strWebSite
-varchar
-50
-ascii_general_ci
-varchar(50)
-es_venues_list
-txtExtraNotes
-blob
-65535
-*/
-
-
-// ToDo: Post venue information
-/*
-Array
-(
-    [locationName] => Camp Impessa
-    [address] => 827559 Township Rd. 8,
-    [city] => Ayr
-    [province] => ON
-    [postalCode] => N3L 3E2
-    [geolocationStyle] => point
-    [geolatitude] => 43.262218
-    [geolongitude] => -80.500216
-    [mapLock] => maplock
-    [phoneNumber] => 519-432-2928
-    [contact] => 
-    [email] => swocamps@scouts.ca
-    [webSite] => camp-impeesa.ca
-    [extraNotes] => FOOL a Scouts Canada site near Ayr Ontario
-Camp Location
-#827559 Township Road 8, Drumbo, ON N0J 1G0
-Township of Blandford-Blenheim, Oxford County
-
-GPS Co-ordinates 43.262218, -80.500216
-    [submit] => Submit
-)
-
-SELECT `COLUMN_NAME` FROM `information_schema`.`COLUMNS`
-WHERE `TABLE_NAME` = 'es_venues_list'
-COLUMN_NAME
-idVenue
-idUser
-dteEntry
-strLocationName
-strAddress
-strCity
-strProvince
-strPostalCode
-strGeolocationStyle
-strGeolocationPoints
-strPhoneNumber
-strContact
-strEmail
-strWebSite
-txtExtraNotes
-*/
-
-// ToDo: Retrieve nearby venue information
 
 
 
